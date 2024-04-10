5d714d7fc890a56ebf72636f410ab463e75b75d9
mihai Bivol <mihaibivol.dev@gmail.com>
2024-04-10 08:34:43 +0300

    Add the edit image functionality to the creation of the route

--- src/main/java/Bouldering_app/domain/Stats.java
+++ src/main/java/Bouldering_app/domain/Stats.java
@@ -1,8 +1,40 @@
 package Bouldering_app.domain;
 
+import java.util.Scanner;
+
 public class Stats {
     public double strength;
     public double technique;
     public double endurance;
     public double flexibility;
+
+    public Stats(double strength, double technique, double endurance, double flexibility) {
+        this.strength = strength;
+        this.technique = technique;
+        this.endurance = endurance;
+        this.flexibility = flexibility;
+    }
+
+    //TODO to string
+    public Stats(){
+        System.out.print("Stats (real number, eg. write 90.1 for 90,1%):\nStrength: ");
+        Scanner myObj = new Scanner(System.in);
+        this.strength = Double.parseDouble(myObj.nextLine());
+        System.out.print("Technique: ");
+        this.technique = Double.parseDouble(myObj.nextLine());
+        System.out.print("Endurance: ");
+        this.endurance = Double.parseDouble(myObj.nextLine());
+        System.out.print("flexibility: ");
+        this.flexibility = Double.parseDouble(myObj.nextLine());
+    }
+
+    public String printForRoute(){
+        return
+                " strength=" + strength +
+                ", technique=" + technique +
+                ", endurance=" + endurance +
+                ", flexibility=" + flexibility;
+    }
+
+
 }
--- src/main/java/Bouldering_app/services/RouteService.java
+++ src/main/java/Bouldering_app/services/RouteService.java
@@ -1,5 +1,113 @@
 package Bouldering_app.services;
 
-public class RouteService {
+import Bouldering_app.domain.*;
 
+import javax.swing.*;
+import java.nio.file.Path;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class RouteService {
+    private static List<Route> routes;
+
+    private static int ID = 0;
+
+    private List<Route> archiveRoutes;
+
+    private static Scanner myObj;
+
+    public RouteService() {
+        this.routes = new ArrayList<>();
+        this.archiveRoutes = new ArrayList<>();
+        myObj = new Scanner(System.in);
+    }
+
+    public Route getRoute(int index){
+        return routes.get(index);
+    }
+    public static void addRouteSetter(User setter){
+        if (setter instanceof Setter) {
+            addRoute();
+        } else {
+            throw new IllegalArgumentException("Only Setter can add pictures");
+        }
+    }
+    private static void addRoute(){
+        System.out.println("---------Add Route---------");
+
+        System.out.print("Grade of the route (eg. : 4 or 5PLUS): ");
+        String input = myObj.nextLine();
+        Grade grade = Grade.valueOf("_" + input); //TODO try except method
+
+        AddImageApp image = new AddImageApp(ID); //image.destinationPath has the full path
+        ID ++;
+
+        System.out.print("Do you want to specify on the image the route? [Y/N]: ");
+        input = myObj.nextLine().toUpperCase();
+        if(input.equals("Y") || input.equals("YES")){
+            SwingUtilities.invokeLater(() -> {
+            editImage app = new editImage(image.destinationPath);
+            app.setVisible(true);
+            });
+        }
+
+        Stats stats = new Stats();
+
+        routes.add(new Route(grade, image.destinationPath, stats));
+    }
+    //get the index from a sorted array of Routes by date
+    public static int chooseRoute(){
+        List<Tuple<Route, Integer>> routes_sorted = new ArrayList<>();
+        for(int i = 0; i < routes.size(); i ++){
+            routes_sorted.add(new Tuple<>(new Route(routes.get(i)), i));
+        }
+        routes_sorted.sort(new RouteDateComparator());
+        for (int i = 0; i < routes.size(); i ++){
+            System.out.println("Index: " + i);
+            System.out.print(routes.get(i).toString() + "\n\n");
+        }
+        System.out.println("Choose a route to see the image by writing the index, or write -1 to exit");
+        int result = Integer.parseInt(myObj.nextLine());
+
+        if (result > routes_sorted.size() || result == -1){
+            return -1;
+        }
+
+        return routes_sorted.get(result).getInteger();
+    }
+
+    public static void showImage(int index){
+        Path path = routes.get(index).getNamePicture();
+        SwingUtilities.invokeLater(() -> {
+            ImageViewer app = null;
+            app = new ImageViewer(path);
+            app.setVisible(true);
+        });
+
+    }
+
+    public void archiveRoute(int index){
+        archiveRoutes.add(routes.remove(index));
+    }
+    public void showArchiveRoutes(){
+         for (Route archiveRoute : archiveRoutes) {
+             archiveRoute.toString();
+         }
+    }
+}
+class Tuple<A,B>{
+    public A route; public B integer;
+
+    public Tuple(A route, B integer) {
+        this.route = route;
+        this.integer = integer;
+    }
+    public A getRoute() {return route;}
+    public B getInteger(){return integer;}
+}
+class RouteDateComparator implements Comparator<Tuple<Route, Integer>> {
+    @Override
+    public int compare(Tuple<Route, Integer> route1, Tuple<Route, Integer> route2) {
+        return route2.getRoute().getDateAdded().compareTo(route1.getRoute().getDateAdded()); // Sort by date in descending order
+    }
 }
